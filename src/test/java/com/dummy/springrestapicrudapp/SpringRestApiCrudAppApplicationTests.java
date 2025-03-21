f19292b99084a786a47aacc98d9295be4c9cdf5a
Ibrahim mohamed <EbrahimMohammed10101@gmail.com>
2025-03-21 02:58:54 +0200

    Initial commit

---
+++ src/test/java/com/dummy/springrestapicrudapp/SpringRestApiCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummy.springrestapicrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
